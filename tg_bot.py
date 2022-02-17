76d1cef62b506f95bdf2b11946bf012c7f97bc9e
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-02-17 21:56:05 +0300

    Create tg bot

---
+++ tg_bot.py
@@ -0,0 +1,154 @@
+import logging
+from enum import Enum
+
+from telegram import ReplyKeyboardMarkup, Bot
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters, ConversationHandler
+)
+
+import static_text
+from tg_logs_handler import TelegramLogsHandler
+from tg_utils import (
+    redis_data,
+    env,
+    check_answer,
+    build_menu,
+    get_quiz_questions,
+    update_user_data
+)
+
+logger = logging.getLogger(__file__)
+
+
+class Conversation(Enum):
+    QUESTION = 1
+    ANSWER = 2
+
+
+def handle_start_message(update, _):
+    chat_id = update.message.chat_id
+    update_user_data(chat_id)
+
+    user = update.effective_user
+    buttons = build_menu(static_text.menu_buttons, n_cols=2)
+    reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+    update.message.reply_text(static_text.start_message.format(first_name=user.first_name), reply_markup=reply_markup)
+
+    return Conversation.QUESTION
+
+
+def handle_cancel_message(update, _):
+    update.message.reply_text(static_text.cancel_message)
+    return ConversationHandler.END
+
+
+def handle_new_question_request(update, context):
+    chat_id = update.message.chat_id
+
+    question_number = redis_data.hget(chat_id, 'question_number')
+    quiz = context.bot_data.get('questions').get(int(question_number))
+    quiz_question = quiz.get('question')
+    quiz_answer = quiz.get('answer')
+    update_user_data(chat_id, current_answer=quiz_answer)
+
+    update.message.reply_text(quiz_question)
+    return Conversation.ANSWER
+
+
+def handle_solution_attempt(update, _):
+    answer = update.message.text
+    chat_id = update.message.chat_id
+    if check_answer(chat_id, answer):
+        update.message.reply_text(static_text.correct_answer_message)
+        update_user_data(chat_id, increase_question_number=True, increase_current_score=True)
+        return Conversation.QUESTION
+    else:
+        update.message.reply_text(static_text.wrong_answer_message)
+
+
+def send_quiz_answer(update, _):
+    chat_id = update.message.chat_id
+    quiz_answer = redis_data.hget(chat_id, 'current_answer')
+
+    message = static_text.quiz_answer_message.format(quiz_answer=quiz_answer)
+    update.message.reply_text(message)
+    update_user_data(chat_id, increase_question_number=True)
+    return Conversation.QUESTION
+
+
+def send_score(update, _):
+    chat_id = update.message.chat_id
+    score = redis_data.hget(chat_id, 'current_score')
+    answers_number = int(redis_data.hget(chat_id, 'question_number')) - 1
+    message = static_text.total_score_message.format(score=score, answers_number=answers_number)
+    update.message.reply_text(message)
+
+
+def handle_unregistered_message(update, _):
+    update.message.reply_text(static_text.unregistered_message)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    tg_token = env.str('TELEGRAM_BOT_TOKEN')
+    tg_dev_token = env.str('TELEGRAM_DEV_BOT_TOKEN')
+    tg_dev_chat_id = env.str('TG_DEV_CHAT_ID')
+
+    dev_bot = Bot(token=tg_dev_token)
+    logs_handler = TelegramLogsHandler(dev_bot, tg_dev_chat_id)
+    logger.addHandler(logs_handler)
+    logger.info('Telegram bot is running')
+
+    updater = Updater(token=tg_token, use_context=True)
+    conv_handler = ConversationHandler(
+        entry_points=[
+            CommandHandler('start', handle_start_message),
+            MessageHandler(Filters.regex('^(Новый вопрос)$') & ~Filters.command,
+                           handle_new_question_request)
+        ],
+        states={
+            Conversation.QUESTION: [
+                MessageHandler(Filters.regex('^(Новый вопрос)$') & ~Filters.command,
+                               handle_new_question_request),
+                MessageHandler(Filters.text & ~Filters.command & ~Filters.regex('^(Мой счет)$'),
+                               handle_unregistered_message)
+            ],
+            Conversation.ANSWER: [
+                MessageHandler(Filters.regex('^(Сдаться)$') & ~Filters.command,
+                               send_quiz_answer),
+                MessageHandler(Filters.text & ~Filters.command & ~Filters.regex('^(Новый вопрос)$') &
+                               ~Filters.regex('^(Мой счет)$'),
+                               handle_solution_attempt),
+                MessageHandler(Filters.regex('^(Новый вопрос)$'),
+                               handle_unregistered_message)
+
+            ],
+        },
+        fallbacks=[
+            CommandHandler('cancel', handle_cancel_message)
+        ]
+    )
+    updater.dispatcher.add_handler(conv_handler)
+    updater.dispatcher.add_handler(MessageHandler(Filters.regex('^(Мой счет)$'), send_score))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command & ~Filters.regex('^(Новый вопрос)$'),
+                                                  handle_unregistered_message))
+
+    updater.dispatcher.bot_data.update(
+        {
+            'questions': get_quiz_questions()
+        }
+    )
+
+    try:
+        updater.start_polling()
+        updater.idle()
+    except Exception as err:
+        logger.error(err)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tg_utils.py
@@ -0,0 +1,75 @@
+import json
+
+import redis
+from environs import Env
+
+env = Env()
+env.read_env()
+
+
+def redis_connection():
+    redis_uri = env.str('REDIS_URL')
+    redis_port = env.str('REDIS_PORT')
+    redis_password = env.str('REDIS_PASSWORD')
+
+    connection = redis.Redis(
+        host=redis_uri,
+        port=redis_port,
+        password=redis_password,
+        decode_responses=True,
+    )
+    if connection.ping():
+        return connection
+    return None
+
+
+redis_data = redis_connection()
+
+
+def check_answer(chat_id, answer):
+    current_answer = redis_data.hget(chat_id, 'current_answer').split('.')[0].split('(')[0].strip()
+    return answer.lower() in current_answer.lower()
+
+
+def build_menu(buttons, n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
+
+
+def get_quiz_questions():
+    with open('quiz_questions.json', 'r') as json_file:
+        quiz_questions = json_file.read()
+    decode_quiz_questions = json.loads(quiz_questions)
+    numbered_questions = {}
+    for number, question in enumerate(decode_quiz_questions, start=1):
+        numbered_questions.update(
+            {
+                number: {
+                    'question': question,
+                    'answer': decode_quiz_questions[question]
+                }
+            }
+        )
+    return numbered_questions
+
+
+def update_user_data(chat_id, increase_question_number=False, current_answer=None, increase_current_score=False):
+    if not redis_data.exists(chat_id):
+        redis_data.hset(chat_id,
+                        mapping={
+                            'question_number': '1',
+                            'current_answer': '',
+                            'current_score': '0'
+                        })
+    if increase_question_number:
+        redis_data.hincrby(chat_id, 'question_number', 1)
+    if increase_current_score:
+        redis_data.hincrby(chat_id, 'current_score', 1)
+    if current_answer:
+        redis_data.hset(chat_id, 'current_answer', current_answer)
